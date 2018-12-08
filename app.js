4534e0e707ff5fc8c8a309a5fa748f2819836db7
Atilla <k.rebien@gmail.com>
2018-12-08 22:29:39 +0100

    starting app starts with notes
    nothing works yet

---
+++ app.js
@@ -0,0 +1,25 @@
+console.log('starting app');
+
+const fs = require('fs');
+const os = require('os');
+const _ = require('lodash');
+const yargs = require('yargs');
+const notes = require('./notes.js');
+
+const argv = yargs.argv;
+var command = process.argv[2];
+console.log('Command:', command);
+console.log('Process', process.argv);
+console.log('Yargs', argv);
+
+if (command === 'add') {
+    notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+    notes.getAll();
+} else if (command === 'read') {
+    notes.getNote(argv.title);
+} else if (command === 'remove') {
+    notes.removeNote(argv.title);
+} else {
+    console.log("Command not found");
+}
